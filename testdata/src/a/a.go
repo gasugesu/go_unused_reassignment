5b2f67b8c29bc6827281fdafdb26e044118f5c95
Shinya Sugamoto <sugamoto@me.com>
2020-09-03 17:00:46 +0900

    fixed test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -8,18 +8,19 @@ var f = func(p int) int{
 	tt := temp
 	a := 987 + tt(p)
 	for i:=0;i<10;i++ {
-		a += i
+		a += i // want "unused"
 	}
 	b := 12345
 	a,b = b,a
 	a += 10
 	if p == 10 {
-		p = 54321
+		p = 54321 // want "unused"
 	}else {
 		a = 39475
 	}
+	p = 12340000 // want "unused"
 	p = a + b
-	a = 12345
+	a = 12345 // want "unused"
 	a += 20 // want "unused"
 	a += 20 // want "unused"
 	a = a + 5124 // want "unused"
