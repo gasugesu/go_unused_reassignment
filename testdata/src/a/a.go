41a39a9f6ad19396f4e4387345ef2a4b22fed56b
Shinya Sugamoto <sugamoto@me.com>
2020-09-03 16:53:35 +0900

    added test data

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,29 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+func temp(a int) int {
+	return a
 }
 
+var f = func(p int) int{
+	tt := temp
+	a := 987 + tt(p)
+	for i:=0;i<10;i++ {
+		a += i
+	}
+	b := 12345
+	a,b = b,a
+	a += 10
+	if p == 10 {
+		p = 54321
+	}else {
+		a = 39475
+	}
+	p = a + b
+	a = 12345
+	a += 20 // want "unused"
+	a += 20 // want "unused"
+	a = a + 5124 // want "unused"
+	a += 30 // want "unused"
+	a += 40 // want "unused"
+	return p
+}
