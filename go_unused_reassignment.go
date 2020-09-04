81c9c1da60552ff1aab9135efe677903e43fc1fe
Shinya Sugamoto <sugamoto@me.com>
2020-09-04 17:24:24 +0900

    temp commit

--- go_unused_reassignment.go
+++ go_unused_reassignment.go
@@ -1,7 +1,6 @@
 package gounusedreassignment
 
 import (
-	"container/list"
 	"fmt"
 	"go/token"
 	"go/types"
@@ -94,7 +93,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 							fmt.Printf("\t\t\t%[1]T\t%[1]v(%[1]p)\n", value)
 						}
 					}
-					mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName)
+					mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName, bf)
 				case *ssa.UnOp:
 					fmt.Println("\t\t", instr.Name(), "Op: ", instr.Op, "Val1: ", instr.X)
 					storeToAddrName := new(string)
@@ -107,7 +106,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						// CommaOk and Op=ARROW の場合ここで前の値が握り潰される
 						*useAddrName = (*value).Name()
 					}
-					mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName)
+					mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName, bf)
 				case *ssa.BinOp:
 					fmt.Println("\t\t", instr.Name(), "Op: ", instr.Op, "Val1: ", instr.X, "Val2: ", instr.Y)
 					storeToAddrName := new(string)
@@ -123,7 +122,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						}
 						if len(*refPtr) > 0 {
 							*useAddrName = (*value).Name()
-							mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName)
+							mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName, bf)
 						}
 					}
 				case *ssa.If:
@@ -138,7 +137,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						}
 						if len(*refPtr) > 0 {
 							*useAddrName = (*value).Name()
-							mgr.storeVarAt(block, nil, instr.Pos(), useAddrName)
+							mgr.storeVarAt(block, nil, instr.Pos(), useAddrName, bf)
 						}
 					}
 				case *ssa.Call:
@@ -157,7 +156,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						}
 						if len(*refPtr) > 0 {
 							*useAddrName = (*value).Name()
-							mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName)
+							mgr.storeVarAt(block, storeToAddrName, instr.Pos(), useAddrName, bf)
 						}
 					}
 				case *ssa.Return:
@@ -171,7 +170,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						}
 						if len(*refPtr) > 0 {
 							*useAddrName = (*value).Name()
-							mgr.storeVarAt(block, nil, instr.Pos(), useAddrName)
+							mgr.storeVarAt(block, nil, instr.Pos(), useAddrName, bf)
 						}
 					}
 				default:
@@ -257,29 +256,157 @@ func (r *blockFlowController) getNextBlock() *ssa.BasicBlock {
 	r.nextBlock[0] = temp
 	return r.nextBlock[0]
 }
+
+func (r *blockFlowController) getCurrentBlock() *ssa.BasicBlock {
+	return r.nextBlock[0]
+}
+
+func (r *blockFlowController) getBlocksPreds() []*ssa.BasicBlock {
+	predsInterfaceSlice := r.blockFlowIn[r.nextBlock[0]].ToSlice()
+	var predsBasicBlock []*ssa.BasicBlock
+	for _, p := range predsInterfaceSlice {
+		p, ok := p.(*ssa.BasicBlock)
+		if !ok {
+			panic(nil)
+		}
+		predsBasicBlock = append(predsBasicBlock, p)
+	}
+	return predsBasicBlock
+}
+
+type varInfo struct {
+	pos                              token.Pos
+	isUsedFromSameBlock              bool
+	isUsedFromBeforeBlock            bool
+	isReassignedBeforeUseInNextBlock bool
+}
+
+func (v *varInfo) isUnused() bool {
+	return false
+}
+
+func (v varInfo) copy() varInfo {
+	return varInfo{
+		pos:                              v.pos,
+		isUsedFromSameBlock:              v.isUsedFromSameBlock,
+		isUsedFromBeforeBlock:            v.isUsedFromBeforeBlock,
+		isReassignedBeforeUseInNextBlock: v.isReassignedBeforeUseInNextBlock,
+	}
+}
+
+type unusedReport struct {
+	pos     token.Pos
+	block   *ssa.BasicBlock
+	message string
+}
+
 func newResubstitutionManager() *ResubstitutionManager {
 	return &ResubstitutionManager{
-		unusedrResubstitution: []token.Pos{},
+		callMap:               make(map[*ssa.BasicBlock]map[string]varInfo),
+		lastUnusedVar:         make(map[*ssa.BasicBlock]map[string][]token.Pos),
+		unusedrResubstitution: []unusedReport{},
 	}
 }
 
 type ResubstitutionManager struct {
-	callMap               map[string]
-	unusedrResubstitution []token.Pos
+	callMap map[*ssa.BasicBlock]map[string]varInfo
+	// 各blockの直前までの使用状況.各ブロックの直前までのブロックで、unusedである変数の情報をもつ
+	lastUnusedVar         map[*ssa.BasicBlock]map[string][]token.Pos
+	unusedrResubstitution []unusedReport
 }
 
-func (r *ResubstitutionManager) storeVarAt(block *ssa.BasicBlock, addrName *string, pos token.Pos, useAddrName *string) {
-	// TODO: 未実装
-	// ここでうまくグラフを構築したい
+func (r *ResubstitutionManager) storeVarAt(block *ssa.BasicBlock, addrName *string, pos token.Pos, useAddrName *string, bf *blockFlowController) {
+	// 方針: 直前のやつをreportする. 最後にunusedかつ未報告のものをまとめてreportする
+	_, ok := r.callMap[block]
+	// もしそのブロック初めての呼び出しであれば直前のブロックまでの情報の全てを統合したmapを作成する
+	if !ok {
+		r.callMap[block] = make(map[string]varInfo)
+		r.lastUnusedVar[block] = make(map[string][]token.Pos)
+		preds := bf.getBlocksPreds()
+		for _, predBlock := range preds {
+			for name := range r.lastUnusedVar[predBlock] {
+				_, ok := r.lastUnusedVar[block][name]
+				if !ok {
+					r.lastUnusedVar[block][name] = make([]token.Pos, 0)
+				}
+				if _, ok := r.callMap[predBlock][name]; ok {
+					// １度は直前のブロックで使用されたがその後の再代入後まだ未使用
+					if r.callMap[predBlock][name].isUsedFromSameBlock == false {
+						r.lastUnusedVar[block][name] = append(r.lastUnusedVar[block][name], r.callMap[predBlock][name].pos)
+					}
+				} else {
+					// 直前のブロックで一度も使用されていないのでその直前ブロック開始時点での未使用情報を付加
+					if _, ok := r.lastUnusedVar[predBlock][name]; ok {
+						r.lastUnusedVar[block][name] = append(r.lastUnusedVar[block][name], r.lastUnusedVar[predBlock][name]...)
+					}
+				}
+			}
+		}
+	}
+	if addrName != nil {
+		v, ok := r.callMap[block][*addrName]
+		// そのブロック内での最初のその変数の割り当ての場合
+		if !ok {
+			// TODO: storeVarAt の直前のblock全てのの*addrnameの使用状況を見てここのisUsedFromSameBlockを制御すべき
 
-}
+			// 直前のブロック見て、そこでunusedであれば直前のブロック内の変数に対してエラー出す
+			preds := bf.getBlocksPreds()
+			for _, predBlock := range preds {
+				vInfo, ok := r.callMap[predBlock][*addrName]
+				if !ok || vInfo.isUsedFromSameBlock {
+					continue
+				}
+				r.unusedrResubstitution = append(r.unusedrResubstitution, unusedReport{vInfo.pos, "Resubstitution before used"})
 
-func (r *ResubstitutionManager) use(block *ssa.BasicBlock, addrName *string) {
-	// TODO: storeでaddrNameにnilを許すとここ不要
+				r.callMap[predBlock][*addrName] = varInfo{
+					pos:                              vInfo.pos,
+					isUsedFromSameBlock:              vInfo.isUsedFromSameBlock,
+					isReassignedBeforeUseInNextBlock: true,
+				}
+			}
+		} else {
+			// 同一ブロック内すでに割り当てありの場合
+			// 同一ブロック内で最後に呼び出された部分でisUsedFromSameBlock==falseであればその直前の変数部分もreportする
+			if !v.isUsedFromSameBlock {
+				r.unusedrResubstitution = append(r.unusedrResubstitution, unusedReport{v.pos, "Resubstitutioned before used"})
+			}
+		}
+		r.callMap[block][*addrName] = varInfo{
+			pos:                              pos,
+			isUsedFromSameBlock:              false,
+			isReassignedBeforeUseInNextBlock: false,
+		}
+	}
+
+	if useAddrName != nil {
+		v, ok := r.callMap[block][*useAddrName]
+		if !ok {
+			// 直前のブロックまでで1つでもunusedであれば
+			preds := bf.getBlocksPreds()
+			for _, predBlock := range preds {
+				vInfo, ok := r.callMap[predBlock][*addrName]
+				if !ok || vInfo.isUsedFromSameBlock {
+					continue
+				}
+				r.unusedrResubstitution = append(r.unusedrResubstitution, unusedReport{vInfo.pos, "Resubstitution before used"})
+
+				r.callMap[predBlock][*addrName] = varInfo{
+					pos:                              vInfo.pos,
+					isUsedFromSameBlock:              vInfo.isUsedFromSameBlock,
+					isReassignedBeforeUseInNextBlock: true,
+				}
+			}
+		}
+		r.callMap[block][*useAddrName] = varInfo{
+			pos:                              v.pos,
+			isUsedFromSameBlock:              true,
+			isReassignedBeforeUseInNextBlock: false,
+		}
+	}
 }
 
 func (r *ResubstitutionManager) calcUnusedrResubstitution() {
-	// TODO: 構築されたグラフを後ろから見て依存関係のないところは全てreportに挿入&削除
+	// TODO: 最後にunusedのもので拾われていないものを全部持ってくる. (そんなものは本当にあるのか？)
 }
 
 func (r *ResubstitutionManager) report() {
